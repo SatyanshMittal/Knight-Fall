85ae2460a1397c8b35cc9eafbeb37cd4527af1a4
Satyansh Mittal <mittalsatyansh@gmail.com>
2024-08-23 03:31:35 +0530

    added a character section in landing page and also fixed the tailwind

--- components/HeroCharacters.tsx
+++ components/HeroCharacters.tsx
@@ -1,11 +1,35 @@
 import React from 'react'
-
+import { CharacterOne } from './character1'
 const  HeroCharacters: React.FC = () => {
   return (
-    <div className='text-red-500 text-7xl  bgwhite w-full '>
+    <div className='h-screen w-full flex flex-col'>
+        <div className='text-3xl mx-auto text-white/80 '>Our Knights</div>
+    <div className=' text-7xl h-screen flex gap-5 justify-center   w-full '>
 
-        hello world
+    {/* <div className='w-1/4 mt-auto '>
+    <CharacterOne/>
+       <div className='w-full h-5 bg-red-600'></div>
+       
+       </div> */}
+
+       
+       <CharacterOne/>
+      
+      
+       <CharacterOne/>
+       
+       <CharacterOne/>
+    {/* <div className='w-1/4 '>
+       <div className='w-full h-5 bg-red-600'></div>
+       <CharacterOne/>
+       </div>
+    <div className='w-1/4 '>
+       <div className='w-full h-5 bg-red-600'></div>
+       <CharacterOne/>
+       </div> */}
+    
         
+         </div>
          </div>
   )
 }
--- components/character1.tsx
+++ components/character1.tsx
@@ -2,10 +2,12 @@ import Spline from '@splinetool/react-spline/next';
 
 export  function CharacterOne() {
   return (
-    <main>
+    <main className=''>
       <Spline
         scene="https://prod.spline.design/10Gkg-vwkK5HOQvT/scene.splinecode" 
+        
       />
+       {/* <div className=' w-full h-5 bg-red-600'/> */}
   </main>
 );
 }
