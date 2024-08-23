fad0ab0cabf9df07a2a47c399150cc5cb97f22a4
Satyansh Mittal <mittalsatyansh@gmail.com>
2024-08-24 00:34:20 +0530

    marquee fixed

--- components/ScrollingCards.tsx
+++ components/ScrollingCards.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import styles from '../styles/ScrollingCards.module.css';
+import Marquee from 'react-fast-marquee';
 
 const characters = [
   { id: 1, name: 'Character 1', video: '/map1.mp4' },
   { id: 2, name: 'Character 2', video: '/map2.mp4' },
   { id: 3, name: 'Character 3', video: '/map3.mp4' },
   { id: 4, name: 'Character 4', video: '/map4.mp4' },
-  { id: 5, name: 'Character 5', video: '/map5.mp4' },
 ];
 
 const maps = [
@@ -14,12 +14,51 @@ const maps = [
   { id: 2, name: 'Map 2', video: '/map2.mp4' },
   { id: 3, name: 'Map 3', video: '/map3.mp4' },
   { id: 4, name: 'Map 4', video: '/map4.mp4' },
-  { id: 5, name: 'Map 5', video: '/map5.mp4' },
 ];
 
-const ScrollingCards: React.FC = () => {
+// const ScrollingCards: React.FC = () => {
+//   return (
+//     <div className={styles.scrollingCards}>
+//       <div className={styles.characters}>
+//         {characters.map((character) => (
+//           <div key={character.id} className={styles.card}>
+//             <video
+//               src={character.video}
+//               autoPlay
+//               loop
+//               muted
+//               className={styles.mapVideo}
+//             />
+//             <p>{character.name}</p>
+//           </div>
+//         ))}
+//       </div>
+//       <div className={styles.maps}>
+//         {maps.map((map) => (
+//           <div key={map.id} className={styles.card}>
+//             <video
+//               src={map.video}
+//               autoPlay
+//               loop
+//               muted
+//               className={styles.mapVideo}
+//             />
+//             <p>{map.name}</p>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ScrollingCards;
+
+
+
+export default function ScrollingCards() {
   return (
-    <div className={styles.scrollingCards}>
+    <div >
+    <Marquee speed={200}>
       <div className={styles.characters}>
         {characters.map((character) => (
           <div key={character.id} className={styles.card}>
@@ -34,7 +73,10 @@ const ScrollingCards: React.FC = () => {
           </div>
         ))}
       </div>
-      <div className={styles.maps}>
+    </Marquee>
+
+    <Marquee direction='right' speed={150}>
+    <div className={styles.maps}>
         {maps.map((map) => (
           <div key={map.id} className={styles.card}>
             <video
@@ -48,8 +90,8 @@ const ScrollingCards: React.FC = () => {
           </div>
         ))}
       </div>
+      </Marquee>
     </div>
-  );
-};
+  )
+}
 
-export default ScrollingCards;
